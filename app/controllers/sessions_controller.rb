1f079726ff071370aa1b3a0315c61a307e1cf9e7
misa <m17thm@gmail.com>
2020-01-20 18:03:56 +0000

    Finish login-function

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,11 +6,18 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # ログイン後に情報ページにリダイレクト
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = '認証に失敗しました。'
       render :new
     end
+  end
+  
+  def destroy
+    log_out
+    flash[:success] = 'ログアウトしました。'
+    redirect_to root_url
   end
 
 end
